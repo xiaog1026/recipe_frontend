01ed0d87bca2a9ba71e75c999af42cf4aa1e5b53
xiaog1026 <xiaog1026@yahoo.co.jp>
2020-10-13 00:33:15 +0900

    主页模块2和模块1的开发

---
+++ lib/widgets/item_count_title.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:lovekitchen/constant/text_size_constant.dart';
+import 'package:lovekitchen/constant/color_constant.dart';
+
+typedef OnClick = void Function();
+
+class ItemCountTitle extends StatelessWidget {
+//  final state = _ItemCountTitleState();
+  final count;
+  final OnClick onClick;
+  final String title;
+  final double fontSize;
+
+  ItemCountTitle(this.title, {Key key, this.onClick, this.count, this.fontSize})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+              child: Text(
+            title,
+            style: TextStyle(
+                fontSize: fontSize == null
+                    ? TextSizeConstant.BookAudioPartTabBar
+                    : fontSize,
+                fontWeight: FontWeight.bold,
+                color: ColorConstant.colorDefaultTitle),
+          )),
+          Text(
+            '全部 ${count == null ? 0 : count} > ',
+            style: TextStyle(
+                fontSize: 12, color: Colors.grey, ),
+          )
+        ],
+      ),
+      onTap: () {
+        if (onClick != null) {
+          onClick();
+        }
+      },
+    );
+  }
+}
